aebf459f4cae34e3c2efd09739093a4e6425ae6d
cof@fastmail.fm <cof@fastmail.fm@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-06 21:18:08 +0000

    Enabled javascript on facts,recommendations

--- trunk/django_magpie/knowledge/admin.py
+++ trunk/django_magpie/knowledge/admin.py
@@ -1,7 +1,10 @@
 from knowledge.models import *
 from django.contrib import admin
 
+class RuleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('facts','recommendations',)
+
 admin.site.register(Fact)
 admin.site.register(Question)
 admin.site.register(Recommendation)
-admin.site.register(Rule)
+admin.site.register(Rule, RuleAdmin)
