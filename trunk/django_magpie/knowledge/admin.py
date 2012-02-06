7e981fb1910d7ba64668fe6d086d67411d109ca7
cof@fastmail.fm <cof@fastmail.fm@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-06 20:23:17 +0000

    Added knowledge

---
+++ trunk/django_magpie/knowledge/admin.py
@@ -0,0 +1,7 @@
+from knowledge.models import *
+from django.contrib import admin
+
+admin.site.register(Fact)
+admin.site.register(Question)
+admin.site.register(Recommendation)
+admin.site.register(Rule)
