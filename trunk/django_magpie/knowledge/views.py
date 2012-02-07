c1f687ace6b6d13cdf4c9fd070b5172bb2f6bf76
cof@fastmail.fm <cof@fastmail.fm@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-07 17:01:12 +0000

    Second attempt at question recommendations

--- trunk/django_magpie/knowledge/views.py
+++ trunk/django_magpie/knowledge/views.py
@@ -1,8 +1,52 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader,RequestContext
 from django.shortcuts import render_to_response
-from knowledge.models import Question
+from knowledge.models import Question,Rule,Fact,Recommendation
 
+# TODO move logic into models
 def index(request):
-    question_list = Question.objects.all()
-    return render_to_response('knowledge/index.html', {'question_list': question_list})
+    if request.method == 'POST':
+        # get list of answers from post form
+        items = request.POST.items() 
+        answers = []
+        for name,value in items:
+            if name.startswith('answer_'):
+                qid = int(name[len('answer_'):])
+                istrue = value == 'y'
+                answers.append((qid, istrue))
+        # convert answers to facts
+        facts = []
+        for qid,istrue in answers:
+            question = Question.objects.get(id=qid)
+            if question:
+                if istrue:
+                    facts.append(question.yes_fact)
+                else:
+                    facts.append(question.no_fact)
+        # match facts to rules
+        rules = []
+        for rule in Rule.objects.all():
+            match_all = True
+            for fact in rule.facts.all():
+                if fact not in facts:
+                    match_all = False
+                    break
+            if match_all:
+                rules.append(rule)
+        # now build list of recommendations
+        rec_list = []
+        for rule in rules:
+            for rec in rule.recommendations.all():
+                if rec not in rec_list:
+                    rec_list.append(rec)
+        # convert to string
+        text_list = []
+        for rec in rec_list:
+            text_list.append(rec.text)
+        return HttpResponse("Thank you for your answers. Recommendations=%s" % ','.join(text_list))
+    else:
+        question_list = Question.objects.all()
+        return render_to_response(
+            'knowledge/index.html', 
+            {'question_list': question_list},
+             context_instance=RequestContext(request))
