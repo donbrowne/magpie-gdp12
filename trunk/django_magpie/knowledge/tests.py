3352725bd741eb5e92557fe6df870e99250280e4
cof@fastmail.fm <cof@fastmail.fm@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-09 17:32:07 +0000

    Added basic tests

--- trunk/django_magpie/knowledge/tests.py
+++ trunk/django_magpie/knowledge/tests.py
@@ -6,11 +6,24 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from knowledge.models import get_ids, get_answers
 
+# dummy class used for tests
+class TestModel:
+    def __init__(self, id):
+        self.id = id
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class KnowledgeTests(TestCase):
+
+    def test_get_ids(self):
+        slist = [ id for id in xrange(10) ]
+        tlist = [TestModel(id) for id in slist]
+        elist = get_ids(tlist)
+        self.assertEqual(slist, elist)
+
+    def test_get_answers(self):
+        slist =  [ (num, True if (num % 2) else False) for num in xrange(10) ] 
+        tlist = [ ( 'answer_%d' % num, 'y' if value else 'n')  for num,value in slist ]
+        elist = get_answers(tlist)
+        self.assertEquals(slist, elist)
+        
