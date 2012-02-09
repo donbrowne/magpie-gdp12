7f2c7a826b0c9984d34959c775de318b88a56061
cof@fastmail.fm <cof@fastmail.fm@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-09 16:59:46 +0000

    Moved business logic from views to models

--- trunk/django_magpie/knowledge/models.py
+++ trunk/django_magpie/knowledge/models.py
@@ -25,3 +25,56 @@ class RuleAnswer(models.Model):
     answer = models.BooleanField()
     def __unicode__(self):
         return self.question.text + ' ' + str(self.answer)
+
+# TODO make a helper class
+def get_ids(alist):
+    return map(lambda elem:elem.id, alist)
+
+def get_answers(items):
+    answers = []
+    for name,value in items:
+        if name.startswith('answer_'):
+            qid = int(name[len('answer_'):])
+            istrue = value == 'y'
+            answers.append((qid, istrue))
+    return answers
+
+# update recommends + calculate next set of questions
+def next_sess(rule_ids, answers, rec_ids):
+    # build list of rules that match all answers
+    rules = []
+    questions = []
+    recommends = list(Recommend.objects.filter(id__in=rec_ids))
+    for rule in Rule.objects.filter(id__in=rule_ids):
+        # check if expected answers match those given
+        match_all = True
+        for ranswer in rule.ruleanswer_set.all():
+            expect = (ranswer.question.id, ranswer.answer)
+            if expect not in answers:
+                match_all = False
+                break
+        if match_all:
+            for recommend in rule.recommends.all():
+                if recommend not in recommends:
+                    recommends.append(recommend)
+            for child_rule in rule.rule_set.all():
+                if child_rule not in rules:
+                    rules.append(child_rule)
+                for ranswer in child_rule.ruleanswer_set.all():
+                    if ranswer.question not in questions:
+                        questions.append(ranswer.question)
+    return rules, questions, recommends
+
+# start a q+a session
+def start_sess():
+    rules = []
+    questions = []
+    recommends = []
+    for rule in Rule.objects.filter(requires__isnull=True):
+        if rule not in rules:
+            rules.append(rule)
+        for ranswer in rule.ruleanswer_set.all():
+            if ranswer.question not in questions:
+                questions.append(ranswer.question)
+    return rules, questions
+
