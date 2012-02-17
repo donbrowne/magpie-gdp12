efc44846b0d447793adc88fb19feaf442f11ca57
cof@fastmail.fm <cof@fastmail.fm@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-17 19:41:40 +0000

    Added iniital question start then ask support

--- trunk/django_magpie/urls.py
+++ trunk/django_magpie/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     #url(r'^$', 'testapp.views.index', name='index'),
     #url(r'^$', 'knowledge.views.index', name='index'),
     url(r'^$', include('knowledge.urls')),
-    # url(r'^django_magpie/', include('django_magpie.foo.urls')),
+    url(r'^knowledge/', include('knowledge.urls')),
+    #url(r'^django_magpie/', include('django_magpie.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
