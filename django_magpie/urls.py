c35ce87f0fc1a41281cbbcbf943ab9de6ecc1613
cof <cof@fastmail.fm>
2012-02-18 13:34:09 +0000

    Added initial support for logging in

--- django_magpie/urls.py
+++ django_magpie/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +10,8 @@ urlpatterns = patterns('',
     #url(r'^$', 'knowledge.views.index', name='index'),
     url(r'^$', include('knowledge.urls')),
     url(r'^knowledge/', include('knowledge.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
     #url(r'^django_magpie/', include('django_magpie.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
