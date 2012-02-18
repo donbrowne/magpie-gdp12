cc089b4e4a2ab9214b36ccfa5c6699e6376e4b73
donmjbrowne@gmail.com <donmjbrowne@gmail.com@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-18 22:55:50 +0000

    Fixed problem where files had to be given a group. Now a file can also be given multiple user groups for access control.
    Cleaned up the recommendation process - the PDF/Vid/PML are now selected from
    files that have been uploaded. New - external links. They are not shown yet

--- trunk/django_magpie/knowledge/admin.py
+++ trunk/django_magpie/knowledge/admin.py
@@ -8,7 +8,13 @@ class FactAdmin(admin.ModelAdmin):
     filter_horizontal = ('requires','recommends',)
     inlines = [FactQuestionInline]
 
+class ExtLinkInline(admin.TabularInline):
+    model = ExternalLink
+
+class RecsAdmin(admin.ModelAdmin):
+    inlines = [ExtLinkInline]
+
 admin.site.register(Fact, FactAdmin)
 admin.site.register(Question)
-admin.site.register(Recommend)
+admin.site.register(Recommend, RecsAdmin)
 admin.site.register(ResourceFile)
