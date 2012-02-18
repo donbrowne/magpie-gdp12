9161f81545f1d3d38f769fade4afb3ec243052dd
cof@fastmail.fm <cof@fastmail.fm@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-18 16:25:53 +0000

    Enabled user registration

--- trunk/django_magpie/registration/urls.py
+++ trunk/django_magpie/registration/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.auth.views import login, logout
+#from django.contrib.auth.views import login, logout
 
-urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+urlpatterns = patterns('django.contrib.auth.views',
+    url(r'^login/$', 'login', name='login'),
+    url(r'^logout/$', 'logout', name='logout'),
+)
+
+urlpatterns += patterns('registration.views',
+    url(r'^register/$', 'register', name='register'),
 )
