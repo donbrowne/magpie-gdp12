b5109ba59d16b6034082b4e465f38e35d6aa9d4e
cof <cof@fastmail.fm>
2012-02-19 22:47:58 +0000

    Made Facts field on UserProfile a js controlled field

--- django_magpie/accounts/admin.py
+++ django_magpie/accounts/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from models import UserProfile
 
 class UserProfileInline(admin.StackedInline):
+    filter_horizontal = ('facts',)
     model = UserProfile
     fk_name = 'user'
     max_num = 1
