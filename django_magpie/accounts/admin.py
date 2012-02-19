7ddb2ebc96903dd38ac530eb18b99856b3026542
cof <cof@fastmail.fm>
2012-02-19 23:08:49 +0000

    Made user profiles a seperate admin

--- django_magpie/accounts/admin.py
+++ django_magpie/accounts/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from models import UserProfile
+from models import UserProfile, UserAnswer
 
+class UserAnswerInline(admin.TabularInline):
+    model = UserAnswer
+    extra = 1
+
+class UserProfileAdmin(admin.ModelAdmin):
+    filter_horizontal = ('facts',)
+    inlines = [UserAnswerInline]
+
+"""
 class UserProfileInline(admin.StackedInline):
     filter_horizontal = ('facts',)
+    inlines = [UserAnswerInline]
     model = UserProfile
     fk_name = 'user'
     max_num = 1
@@ -14,3 +24,6 @@ class UserProfileAdmin(UserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, UserProfileAdmin)
+"""
+
+admin.site.register(UserProfile,  UserProfileAdmin)
