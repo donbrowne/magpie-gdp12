c9f14c631f9dec8cbea5252ebc930be781656619
cof <cof@fastmail.fm>
2012-02-19 22:30:20 +0000

    First attempt at UserProfiles

--- django_magpie/accounts/models.py
+++ django_magpie/accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from knowledge.models import Fact,Question
 
-# Create your models here.
+"""
+class UserType(models.Model):
+    name = models.CharField(max_length=30)
+    facts = models.ManyToManyField('Fact',blank=True)
+"""
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    facts = models.ManyToManyField(Fact,blank=True)
+    def __unicode__(self):
+        return self.user.username
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(UserProfile)
+    question = models.ForeignKey(Question)
+    answer = models.BooleanField()
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        print type(instance)
+        print str(instance)
+        print 'creating user here'
+        UserProfile.objects.create(user=instance)
+
+# tell us if a new user created
+post_save.connect(create_user_profile, sender=User)
