e6febb22ed2c77a4a8471a490223a54e69336696
donmjbrowne@gmail.com <donmjbrowne@gmail.com@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-22 17:46:15 +0000

    Added installation scripts. Modified instructions accordingly

---
+++ trunk/setup.sh
@@ -0,0 +1,6 @@
+mkdir sqlite3
+mkdir resources
+cd ./pml
+make
+make clean
+cd ..
---
+++ trunk/setup_tenfour.sh
@@ -0,0 +1,4 @@
+add-apt-repository ppa:fkrull/deadsnakes
+apt-get update
+apt-get install python2.7 graphviz python-distribute-deadsnakes
+easy_install-2.7 pydot django
