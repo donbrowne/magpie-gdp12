472c2515603669038bf47ffc9f0d9f41f9f075ba
donmjbrowne@gmail.com <donmjbrowne@gmail.com@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-02-23 23:58:17 +0000

    Enhanced setup_tenfour - now automatically builds/installs mod_wsgi for Python 2.7, does not prompt users during package installation, and prevents non root user from running script.

--- trunk/setup_tenfour.sh
+++ trunk/setup_tenfour.sh
@@ -1,4 +1,20 @@
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+else
+   echo "Setting up Python environment for Ubuntu 10.04"
+fi
 add-apt-repository ppa:fkrull/deadsnakes
 apt-get update
-apt-get install python2.7 graphviz python-distribute-deadsnakes
+apt-get --yes install python2.7 graphviz python-distribute-deadsnakes apache2-threaded-dev python2.7-dev
 easy_install-2.7 pydot django
+wget http://modwsgi.googlecode.com/files/mod_wsgi-3.3.tar.gz
+tar -xzvf mod_wsgi-3.3.tar.gz
+cd mod_wsgi-3.3
+./configure --with-python=/usr/bin/python2.7
+make
+make install
+make clean
+/etc/init.d/apache2 reload
+cd ..
+rm -rf mod_wsgi-3.3*
