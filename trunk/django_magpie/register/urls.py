37aa71995ff73f7a0c31c85879a00afa0fba0791
donmjbrowne@gmail.com <donmjbrowne@gmail.com@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-03-04 10:34:14 +0000

    Added custom logout view to ensure redirection to index page

--- trunk/django_magpie/register/urls.py
+++ trunk/django_magpie/register/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('django.contrib.auth.views',
     url(r'^login/$', 'login', name='login'),
-    url(r'^logout/$', 'logout', name='logout'),
 )
 
 urlpatterns += patterns('register.views',
     url(r'^register/$', 'register', name='register'),
     url(r'^account/$', 'account', name='account'),
+)
+
+urlpatterns += patterns('knowledge.views',
+    url(r'^logout/$', 'logout_view', name='logout'),
 )
