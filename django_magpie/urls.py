ae8f838a9664cdfa2cb7a982b7faabffef183098
donmjbrowne <donmjbrowne@gmail.com>
2012-04-06 16:53:16 +0000

    Changed favicon implementation so it works with proisis. (It's not in the default location on proisis)

--- django_magpie/urls.py
+++ django_magpie/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,11 +9,5 @@ urlpatterns = patterns('',
     url(r'^$', 'knowledge.views.index', name='index'),
     url(r'^knowledge/', include('knowledge.urls')),
     url(r'^register/', include('register.urls')),
-    #url(r'^django_magpie/', include('django_magpie.foo.urls')),
-    url(r'^favicon\.ico$',  'django.views.generic.simple.redirect_to',  {'url': settings.STATIC_URL+'img/favicon.ico'}),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
