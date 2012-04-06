a691a8764ade7bd26bfde468b8c696604ab03ad0
donmjbrowne <donmjbrowne@gmail.com>
2012-04-06 16:43:27 +0000

    Added a favicon. If nothing else, it will help me find my runserver tab when I have a million tabs open...

--- django_magpie/urls.py
+++ django_magpie/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^knowledge/', include('knowledge.urls')),
     url(r'^register/', include('register.urls')),
     #url(r'^django_magpie/', include('django_magpie.foo.urls')),
-
+    url(r'^favicon\.ico$',  'django.views.generic.simple.redirect_to',  {'url': settings.STATIC_URL+'img/favicon.ico'}),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
