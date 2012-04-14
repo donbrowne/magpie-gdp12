3de54be1319c76697a078feee946f1cc15fa1ae6
donmjbrowne@gmail.com <donmjbrowne@gmail.com@8e648570-9298-396f-82b9-9ee2e509cc35>
2012-04-14 16:34:07 +0000

    Added unit test for escapeForJS, begin annotating function as being unit tested.

--- trunk/django_magpie/knowledge/templatetags/customFilters.py
+++ trunk/django_magpie/knowledge/templatetags/customFilters.py
@@ -2,18 +2,22 @@ from django import template
 
 register = template.Library()
 
+#Unit tested
 def lslice(string, amount):
     return string[int(amount):]
     
+#Unit tested
 def contains(string, substring):
     return substring in string
     
+#Unit tested
 def isImg(string):
     types = ['jpg','jpeg','png','svg','gif','bmp']
     stringArray = string.split(".")[-1:]
     extension = stringArray[0].lower()
     return extension in types
 
+#Unit tested
 def escapeForJS(string):
     chars = [' ','"','\'','(',')','<','>','[',']']
     for c in chars:
