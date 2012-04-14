ab4b98645b77705dd02479a98235e0e7dda2d796
cof <cof@fastmail.fm>
2012-04-14 17:40:35 +0000

    No longer used

--- django_magpie/testapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-    
-urlpatterns = patterns('testapp.views',
-   # url(r'^add_rule/(?P<ruleset_id>\d+)/$', 'add_rule', name="add_rule"),
-    url(r'^$', 'index'),
-    url(r'^ask$', 'ask'),
-    url(r'^done$', 'done'),
-    url(r'^state$', 'saved', name='saved'),
-)
